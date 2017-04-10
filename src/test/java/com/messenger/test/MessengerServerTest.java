65de8e5dc31fc787b22d6b630e6471b456b88c8a
Mansi Mahendru <mamahendru@paypal.com>
2017-04-09 21:48:12 -0700

    tests added

---
+++ src/test/java/com/messenger/test/MessengerServerTest.java
@@ -0,0 +1,16 @@
+package com.messenger.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by mamahendru on 4/9/17.
+ */
+//TODO need to add tests for all using in process server
+public class MessengerServerTest {
+    @Test
+    public void register() {
+        String str = "Junit is working fine";
+        assertEquals("Junit is working fine",str);
+    }
+}
